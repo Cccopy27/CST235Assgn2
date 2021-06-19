18fe4a19d312a60b634309f71a2897c705f3fcaf
richardlee9966 <richardlee9966@gmail.com>
2021-06-19 14:44:27 +0800

    initial

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Notes</title>
+    
+</head>
+<body class="bg">
+    <header>
+        <nav>
+            <a href="index.php" class="notes">Notes</a>
+        </nav>
+</header>
