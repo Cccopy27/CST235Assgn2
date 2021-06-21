afa3739624ab7b1dab4597c7f77e2bba77d1ab93
Cccopy27 <wenhao0627@student.usm.my>
2021-06-21 16:21:48 +0800

    login limit

--- login.php
+++ login.php
@@ -2,6 +2,22 @@
 
 <?php
     session_start();
+    //check login attempts
+    
+    if(isset($_SESSION['locked'])){
+        $_SESSION['diff'] = time() - $_SESSION['locked'];
+        if($_SESSION['diff'] > 30){
+            unset($_SESSION['locked']);
+            $_SESSION['login_attempts'] = 0;
+        }
+    }
+    // initialize session login attempts 
+    else if(!isset($_SESSION['login_attempts'])){
+        $_SESSION['login_attempts'] = 0;
+    }
+
+    
+
     //connect to databse
 	$conn = mysqli_connect('localhost', 'kuro7799', 'wenhao0627', 'noteboard');
 
@@ -69,17 +85,15 @@
 
                 // redirect to next page
                 header('Location: index.php');
-                //exit();
             }
             else{
+                //update login attempts
+                $_SESSION['login_attempts'] += 1;
+               
 			    $errors['username'] = 'Wrong username or password';
                 $sql = "INSERT INTO userLog(user,login_status) VALUES('$user','D')";
                 mysqli_query($conn, $sql);
 
-                // close connection
-                mysqli_close($conn);
-
-                //exit();
             }
         }
     }
@@ -108,7 +122,28 @@
             <div class="red-text">
                 <?php echo $errors['password']; ?>
             </div>
+            <?php 
+                // login fail > 3
+                if (isset($_SESSION['login_attempts']) && $_SESSION['login_attempts'] > 2){
+                    echo '<p style="color:white;font-size:18px;">You have exceeded the maximum login attempts!</p>' ;
+                    echo '<p style="color:white;font-size:18px; display:inline">Please try again after </p>';
+                    
+                    // get time when fail login for one time only
+                    if(!isset($_SESSION['locked'])){
+                        $_SESSION['locked'] = time();
+                        echo '<p style="color:white;font-size:18px;display:inline">30 seconds</p>';
+
+                    // countdown time for 30s
+                    }else{
+                        $timeleft = 30 - $_SESSION['diff'];
+                        echo '<p style="color:white;font-size:18px;display:inline">'.$timeleft .' seconds</p>';
+                    }
+                // login fail <= 3
+                }else{ 
+            ?>
+             
 			<input type="submit" value="Login" name="submit" class="btn-login"/>
+            <?php } ?>
 		</form>
 	</div>
 </body>
